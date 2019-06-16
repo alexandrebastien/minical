f0ce0a3bb811c308c928dd13e94d278b91dfcd3a
daiju <daiju.azuma@gmail.com>
2019-06-16 15:22:29 +0900

    add Japanese lang files

---
+++ lang/ja/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Japanese settings file for DokuWiki Plugin minical
+ * @author Daiju Azuma <cdaiju.azuma@gmail.com>
+ */
+ 
+$lang['current']    = '今月';
+$lang['go']         = 'Go';
+
+$lang['year']       = '年';
+$lang['mon']        = '月';
+ 
+$lang['days'][1]    = '月';
+$lang['days'][2]    = '火';
+$lang['days'][3]    = '水';
+$lang['days'][4]    = '木';
+$lang['days'][5]    = '金';
+$lang['days'][6]    = '土';
+$lang['days'][7]    = '日';
+ 
+$lang['month'][1]   = '１月';
+$lang['month'][2]   = '２月';
+$lang['month'][3]   = '３月';
+$lang['month'][4]   = '４月';
+$lang['month'][5]   = '５月';
+$lang['month'][6]   = '６月';
+$lang['month'][7]   = '７月';
+$lang['month'][8]   = '８月';
+$lang['month'][9]   = '９月';
+$lang['month'][10]  = '10月';
+$lang['month'][11]  = '11月';
+$lang['month'][12]  = '12月';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
