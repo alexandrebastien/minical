943900e0af9cc90c3c5e8c62cd7f395238bbe399
Kali Chip <kali@tinselcity.net>
2021-11-30 13:32:02 +0100

    Added Spanish translation

---
+++ lang/es/lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Spanish Language File
+ * @author Kali Chip <kali@tinselcity.net>
+ */
+ 
+$lang['current']    = 'Actual';
+$lang['go']         = 'Ir';
+
+$lang['year']       = 'Año';
+$lang['mon']        = 'Mes';
+ 
+$lang['days'][1]    = 'L';
+$lang['days'][2]    = 'M';
+$lang['days'][3]    = 'X';
+$lang['days'][4]    = 'J';
+$lang['days'][5]    = 'V';
+$lang['days'][6]    = 'S';
+$lang['days'][7]    = 'D';
+ 
+$lang['month'][1]   = 'Enero';
+$lang['month'][2]   = 'Febrero';
+$lang['month'][3]   = 'Marzo';
+$lang['month'][4]   = 'Abril';
+$lang['month'][5]   = 'Mayo';
+$lang['month'][6]   = 'Junio';
+$lang['month'][7]   = 'Julio';
+$lang['month'][8]   = 'Agosto';
+$lang['month'][9]   = 'Septiembre';
+$lang['month'][10]  = 'Octubre';
+$lang['month'][11]  = 'Noviembre';
+$lang['month'][12]  = 'Diciembre';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
---
+++ lang/es/settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Spanish settings file for DokuWiki Plugin minical
+ * @author Kali Chip <kali@tinselcity.net>
+ */
+$lang['weekstart'] = 'Día de comienzo de la semana';
+$lang['timezone']  = 'Zona horaria (por defecto usa la del servidor)';
